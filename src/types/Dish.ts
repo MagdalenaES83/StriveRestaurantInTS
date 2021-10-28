bdbbb74607f21b74938396fcea6788030efbd04e
MagdalenaES83 <84572667+MagdalenaES83@users.noreply.github.com>
2021-10-28 16:31:57 +0200

    StriveRes in TS

---
+++ src/types/Dish.ts
@@ -0,0 +1,21 @@
+interface Comment {
+  id: number;
+  rating: number;
+  comment: string;
+  author: string;
+  date: string;
+}
+
+interface Dish {
+  id: number;
+  name: string;
+  image: string;
+  category: string;
+  label: string;
+  price: string;
+  description: string;
+  comments: Comment[];
+}
+
+export default Dish;
+
